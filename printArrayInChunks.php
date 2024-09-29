e26e79f7a61a9a01bbcb81d46cac14796f3b8573
kennedy mokua <mogakakennedy254@gmail.com>
2024-09-29 22:34:16 +0300

    added new files

--- printArrayInChunks.php
+++ printArrayInChunks.php
@@ -1,14 +1,15 @@
 <?php
 
-function printArrayInChunks($array, $batchSize) { //This defines a function printArrayInChunks which takes two parameters
-    $batches = array_chunk($array, $batchSize); //This is a built-in PHP function that splits an array into chunks.
-    foreach ($batches as $batchIndex => $batch) { //loop iterates over each batch of the chunked array
-        foreach ($batch as $index => $value) { //loop iterates over each element of the current chunk
-            echo "batch " . $batchIndex . " item " . $index . " - value: " . $value . "\n"; //This prints out the batch number, item number within that batch, and the value of the current item, followed by a newline
+function printArrayInChunks($array, $batchSize) {
+    $batches = array_chunk($array, $batchSize);
+//    print_r($batches);
+
+    foreach ($batches as $batchIndex => $batch) {
+        foreach ($batch as $index => $value) {
+            echo "batch " . $batchIndex . " item " . $index . " - value: " . $value . "\n";
         }
     }
 }
-
-$array = range(1, 20); //creates an array of numbers from 1 to 20.
-$batchSize = 5; //array will be chunked into groups of 5 elements.
-printArrayInChunks($array, $batchSize); //This calls the printArrayInChunks function with the created array and batch size.
+$array = range(1, 20);
+$batchSize = 5;
+printArrayInChunks($array, $batchSize);
